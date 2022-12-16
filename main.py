3a67d4864c334a7b20c40bd474ae71ba604db3c9
Vishesh Pandey <116044800+visheshpandey2001@users.noreply.github.com>
2022-12-16 17:22:58 +0000

    created functions

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ pygame.init()
 DISPLAY = pygame.display.set_mode((400, 400))
 pygame.display.set_caption('Car Racing Game')
 
+# colors
+BLACK = (0, 0, 0)
 
 class Road():
     def __init__(self,x,y):
@@ -38,8 +40,6 @@ class Car():
 
 car1 = Car(100,100)
 
-BLACK = (0, 0, 0)
-
 road1 = Road(0,0)
 road2 = Road(0,100)
 road3 = Road(0,200)
@@ -47,19 +47,30 @@ road4 = Road(0,300)
 road5 = Road(0,400)
 road6 = Road(0,500)
 
+roads = [road1,road2,road3,road4,road5,road6]
+
 clock = pygame.time.Clock()
 
-while True:
-    clock.tick(60)
-    DISPLAY.fill(BLACK)
+# functions
+
+def displayRoads():
     DISPLAY.blit(road1.road, (0,road1.y))
     DISPLAY.blit(road2.road, (0,road2.y))
     DISPLAY.blit(road3.road, (0,road3.y))
     DISPLAY.blit(road4.road, (0,road4.y))
     DISPLAY.blit(road5.road, (0,road5.y))
     DISPLAY.blit(road6.road, (0,road6.y))
+
+def displayCars():
     car1.display()
 
+def shiftRoad():
+    for road in roads:
+        road.y += 1 
+        if road.y > 400:
+            road.y = -road.road.get_height()
+
+def handleMovement():
     for event in pygame.event.get():
         if event.type == QUIT:
             pygame.quit()
@@ -81,11 +92,12 @@ while True:
             if event.key == pygame.K_s:
                 if car1.y < DISPLAY.get_height() - car1.car.get_height():
                     car1.move_down()
-
-    roads = [road1,road2,road3,road4,road5,road6]
-    for road in roads:
-        road.y += 1 
-        if road.y > 400:
-            road.y = -road.road.get_height()
-  
+    
+while True:
+    clock.tick(60)
+    DISPLAY.fill(BLACK)
+    displayRoads()
+    displayCars()
+    handleMovement()
+    shiftRoad()
     pygame.display.update()
